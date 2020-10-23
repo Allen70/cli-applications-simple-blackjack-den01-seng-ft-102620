56ad10dcab84a38fbd457ef52c8fcdad4cdd37fa
Allen Richard Lea <allenlea2007@gmail.com>
2020-10-23 01:55:54 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,61 @@
+require 'pry'
+
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  rand(1..11).to_i
 end
 
-def display_card_total
+def display_card_total (total)
   # code #display_card_total here
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
+def end_game (total)
   # code #end_game here
+  puts "Sorry, you hit #{total}. Thanks for playing!"
 end
 
 def initial_round
   # code #initial_round here
+  total = deal_card + deal_card
+  display_card_total(total)
+  total
 end
 
-def hit?
-  # code hit? here
+def hit? (input)
+  prompt_user
+  
+  answer = get_user_input
+  if answer == "h"
+    input = input + deal_card
+  elsif answer == "s"
+  
+  else
+    invalid_command
+    prompt_user
+    get_user_input
+  end
+  input
 end
+
 
 def invalid_command
   # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -40,5 +64,13 @@ end
 
 def runner
   # code runner here
+  welcome
+  total = initial_round
+  
+  if total<21
+    hit?(total)
+    display_card_total(total)
+  else 
+    end_game
+  end
 end
-    
